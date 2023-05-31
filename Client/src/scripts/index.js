33f86fb19fbcc063596d8a87abe4800cde843806
deni2403 <deni240397@gmail.com>
2023-05-31 16:29:12 +0700

    Initialize Sehati-Id Project

---
+++ Client/src/scripts/index.js
@@ -0,0 +1,4 @@
+import 'regenerator-runtime';
+import * as bootstrap from 'bootstrap';
+import '../styles/main.scss';
+import '../styles/responsive.scss';
---
+++ Client/webpack.common.js
@@ -0,0 +1,55 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/scripts/index.js'),
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: () => [
+                  autoprefixer,
+                ],
+              },
+            },
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: path.resolve(__dirname, 'src/template/index.html'),
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/public/'),
+          to: path.resolve(__dirname, 'dist/'),
+        },
+      ],
+    }),
+  ],
+};
